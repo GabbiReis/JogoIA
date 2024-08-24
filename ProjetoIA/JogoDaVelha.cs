f5665551e0bbe03e4d095685b7e2e57693c3415a
GabbiReis <gabrielastefanereis@gmail.com>
2024-08-24 17:54:45 -0300

    Levando arquivos do Jogo

---
+++ ProjetoIA/JogoDaVelha.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIA
+{
+    public class JogoDaVelha
+    {
+        private char[,] tabuleiro = new char[3, 3];
+        private char jogador = 'X';
+        private char computador = 'O';
+
+        public int CalcularPontuacao(int linha, int coluna)
+        {
+            int pontos = 0;
+
+            if (linha == 1 && coluna == 1) pontos += 2;
+
+            if ((linha == 0 || linha == 2) && (coluna == 0 || coluna == 2)) pontos += 1;
+
+            if (VerificarAmeaca(linha, coluna)) pontos -= 2;
+
+            if (ImpedeVitoriaAdversario(linha, coluna)) pontos += 4;
+
+            if (LevaAVitoria(linha, coluna)) pontos += 4;
+
+            return pontos;
+        }
+
+        private bool VerificarAmeaca(int linha, int coluna)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (tabuleiro[linha, i] == jogador || tabuleiro[i, coluna] == jogador) return true;
+            }
+            if (linha == coluna)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (tabuleiro[i, i] == jogador) return true;
+                }
+            }
+            if (linha + coluna == 2)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (tabuleiro[i, 2 - i] == jogador) return true;
+                }
+            }
+            return false;
+        }
+        private bool ImpedeVitoriaAdversario(int linha, int coluna)
+        {
+            tabuleiro[linha, coluna] = computador;
+            bool impede = VerificaVitoria(jogador);
+            tabuleiro[linha, coluna] = '\0';
+            return impede;
+        }
+
+        public bool LevaAVitoria(int linha, int coluna)
+        {
+            tabuleiro[linha, coluna] = computador;
+            bool vitoria = VerificaVitoria(computador);
+            tabuleiro[linha, coluna] = '\0';
+            return vitoria;
+        }
+
+        public bool VerificaVitoria(char jogador)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (tabuleiro[i, 0] == jogador && tabuleiro[i, 1] == jogador && tabuleiro[i, 2] == jogador) return true;
+                if (tabuleiro[0, i] == jogador && tabuleiro[1, i] == jogador && tabuleiro[2, i] == jogador) return true;
+            }
+            if (tabuleiro[0, 0] == jogador && tabuleiro[1, 1] == jogador && tabuleiro[2, 2] == jogador) return true;
+            if (tabuleiro[0, 2] == jogador && tabuleiro[1, 1] == jogador && tabuleiro[2, 0] == jogador) return true;
+
+            return false;
+        }
+
+        public (int, int) EscolherMelhorPosicao()
+        {
+            int melhorPontuacao = int.MinValue;
+            (int, int) melhorPosicao = (-1, -1);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (tabuleiro[i, j] == '\0')
+                    {
+                        int pontuacao = CalcularPontuacao(i, j);
+                        if (pontuacao > melhorPontuacao)
+                        {
+                            melhorPontuacao = pontuacao;
+                            melhorPosicao = (i, j);
+                        }
+                    }
+                }
+            }
+            return melhorPosicao;
+        }
+
+        public void Jogar(int linha, int coluna)
+        {
+            if (tabuleiro[linha, coluna] == '\0')
+            {
+                tabuleiro[linha, coluna] = jogador;
+            }
+        }
+
+        public void JogarComputador()
+        {
+            var (linha, coluna) = EscolherMelhorPosicao();
+            tabuleiro[linha, coluna] = computador;
+        }
+
+        public char[,] ObterTabuleiro()
+        {
+            return tabuleiro;
+        }
+
+        public bool TabuleiroCheio()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (tabuleiro[i, j] == '\0') return false;
+                }
+            }
+            return true;
+        }
+    }
+
+
+}
---
+++ ProjetoIA/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            JogoDaVelha jogo = new JogoDaVelha();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Menu do Jogo da Velha ===");
+                Console.WriteLine("1. Jogar contra o Computador");
+                Console.WriteLine("2. Sair");
+                Console.Write("Escolha uma opção: ");
+                string escolha = Console.ReadLine();
+
+                if (escolha == "1")
+                {
+                    JogarContraComputador(jogo);
+                }
+                else if (escolha == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida! Pressione qualquer tecla para tentar novamente.");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        static void JogarContraComputador(JogoDaVelha jogo)
+        {
+            bool fimDeJogo = false;
+
+            while (!fimDeJogo)
+            {
+                Console.Clear();
+                ExibirTabuleiro(jogo);
+                Console.WriteLine("Seu turno. Escolha uma posição ( Primeiro a linha e depois a coluna): ");
+
+                int linha = -1, coluna = -1;
+                while (linha < 0 || linha > 2 || coluna < 0 || coluna > 2)
+                {
+                    Console.Write("Linha entre as opções (0, 1, 2): ");
+                    linha = int.Parse(Console.ReadLine());
+                    Console.Write("Coluna entre as opções (0, 1, 2): ");
+                    coluna = int.Parse(Console.ReadLine());
+                }
+
+                jogo.Jogar(linha, coluna);
+
+                if (jogo.VerificaVitoria('X'))
+                {
+                    Console.Clear();
+                    ExibirTabuleiro(jogo);
+                    Console.WriteLine("Você venceu!");
+                    fimDeJogo = true;
+                }
+                else if (jogo.TabuleiroCheio())
+                {
+                    Console.Clear();
+                    ExibirTabuleiro(jogo);
+                    Console.WriteLine("Empate!");
+                    fimDeJogo = true;
+                }
+                else
+                {
+                    jogo.JogarComputador();
+
+                    if (jogo.VerificaVitoria('O'))
+                    {
+                        Console.Clear();
+                        ExibirTabuleiro(jogo);
+                        Console.WriteLine("O computador venceu!");
+                        fimDeJogo = true;
+                    }
+                    else if (jogo.TabuleiroCheio())
+                    {
+                        Console.Clear();
+                        ExibirTabuleiro(jogo);
+                        Console.WriteLine("Empate!");
+                        fimDeJogo = true;
+                    }
+                }
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu.");
+            Console.ReadKey();
+        }
+
+        static void ExibirTabuleiro(JogoDaVelha jogo)
+        {
+            char[,] tabuleiro = jogo.ObterTabuleiro();
+
+            Console.Clear();
+            Console.WriteLine("   0   1   2 ");
+            Console.WriteLine("0  {0} | {1} | {2}", Formatar(tabuleiro[0, 0]), Formatar(tabuleiro[0, 1]), Formatar(tabuleiro[0, 2]));
+            Console.WriteLine("  ---|---|---");
+            Console.WriteLine("1  {0} | {1} | {2}", Formatar(tabuleiro[1, 0]), Formatar(tabuleiro[1, 1]), Formatar(tabuleiro[1, 2]));
+            Console.WriteLine("  ---|---|---");
+            Console.WriteLine("2  {0} | {1} | {2}", Formatar(tabuleiro[2, 0]), Formatar(tabuleiro[2, 1]), Formatar(tabuleiro[2, 2]));
+        }
+
+        static char Formatar(char c)
+        {
+            return c == '\0' ? ' ' : c;
+        }
+    }
+}
